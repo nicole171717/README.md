2225eea6c6b3461cfcf2d78740a38db690615c26
nicole171717 <nidi5169@colorado.edu>
2024-11-05 20:42:29 -0700

    Update and rename README.md to HealthcareAppAssistant.cpp

---
+++ HealthcareAppAssistant.cpp
@@ -0,0 +1,32 @@
+# Healthcare App Assistant
+
+# Description
+
+The Healthcare App Assistant is a C++ project designed to track medical symptoms, provide virtual health support, and manage healthcare information. It's goal is to help patients figure out a possible diagnosis by inputting symptoms, manage doctors appointments, and set up reminders for medications. 
+
+# Installation 
+
+1. **Clone repository**
+
+   ```
+   git clone https://github.com/your-username/Healthcare-App-Assistant.git
+
+2. navigate to directory
+cd HealthcareAppAssistant
+
+3. compile
+g++ -o HealthcareAppAssistant.cpp
+
+4. run
+./a.out
+
+# usage
+
+1. prompt user to enter symptoms
+2. system will analyze and provide a list of possible conditions
+3. prompted to optionally set reminders for medication
+4. promted to optionally set reminders for future medical appointments
+
+
+
+
